e27923bb136dd0d5b157820818ce5e93e36bb111
Joshua A. Anderson <joaander@umich.edu>
2018-07-06 09:01:46 -0400

    Fix typo

--- recipe/build.sh
+++ recipe/build.sh
@@ -6,7 +6,7 @@ export TBB_LINK=${PREFIX}/lib
 cmake ../ \
       -DCMAKE_INSTALL_PREFIX=${SP_DIR} \
       -DPYTHON_EXECUTABLE=${PYTHON} \
-      -Dembree_DIR=${PREFIX}/lib/cmake/embree-3.2.0
+      -Dembree_DIR=${PREFIX}/lib/cmake/embree-3.2.0 \
       -DENABLE_EMBREE=on \
       -DENABLE_OPTIX=off
 
